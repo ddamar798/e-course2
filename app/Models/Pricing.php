cdb29d26a838a4293e6c108b422d810436558ce9
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-02-16 19:13:52 +0700

    feat: edit Pricing Models

--- app/Models/Pricing.php
+++ app/Models/Pricing.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Pricing extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'duration',
+        'price',
+    ];
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(transaction::class);
+    }
+
+    // Agar paket yang aktif tidak dapat dibeli lagi.
+    public function isSubscribedByUser($userId)
+    {
+        return $this->transactions()
+        ->where('user_id', $userId)
+        ->where('is_paid', true) // Only consider paid subscriptions.
+        ->where('ended_at', '>=', now())
+        ->exists();
+    }
 }
