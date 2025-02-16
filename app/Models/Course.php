4aeb31fd6b78e2bd2e1f7108d6baf8a593241db0
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-02-16 19:08:13 +0700

    feat: edit Course Models

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -3,8 +3,59 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Course extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'slug',
+        'name',
+        'tumbnail',
+        'about',
+        'isz-popular',
+        'category_id',
+    ];
+
+    public function setNameAttribute($value){
+        $this->attributes['name'] = $value;
+        $this->attribute['slug'] = Str::slug($value); // use Illuminate\Support\Str;
+    }
+
+    public function benefits(): HasMany
+    {
+        return $this->HasMny(CourseBenefit::class);
+    }
+
+    public function courseSections(): HasMany
+    {
+        return $this->hasMany(CourseSection::class);
+    }
+
+    public function courseStudents(): HasMany
+    {
+        return $this->hasMany(courseStudent::class, 'course_id');
+    }
+
+    public function courseMentors(): HasMany
+    {
+        return $this->hasMany(CourseMentor::class, 'course_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(category::class, 'category_id');
+    }
+
+      // Menghitung kelas ini punya berapa section.
+    public function getContentAtribute()
+    {
+     return $this->courseSection->sum(function ($section){
+            return $section->sectionContents->count();
+        });
+    }
 }
